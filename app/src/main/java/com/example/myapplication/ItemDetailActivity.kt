ecf9f47dab29bc41af4d136158431065e9328494
Khalid Ahmed <a@b.c>
2021-01-01 23:50:08 -0500

    'Changes'

--- app/src/main/java/com/example/myapplication/ItemDetailActivity.kt
+++ app/src/main/java/com/example/myapplication/ItemDetailActivity.kt
@@ -2,10 +2,13 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import android.view.MenuItem
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 /**
  * An activity representing a single Item detail screen. This
@@ -20,6 +23,12 @@ class ItemDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_item_detail)
         setSupportActionBar(findViewById(R.id.detail_toolbar))
 
+        //Azure
+        AppCenter.start(
+            application, "d8756845-0efa-47c3-ad26-4d15c6ca420e",
+            Analytics::class.java, Crashes::class.java
+        )
+        
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
